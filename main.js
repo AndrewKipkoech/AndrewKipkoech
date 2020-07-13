8e75905a316e665542460ad099f6864c0cfa2fd8
Andrew Kipkoech <andrewkp78@gmail.com>
2020-07-13 18:16:37 -0400

    Tech progress bar

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ $(document).ready(function(){
   $('.menu-toggler').on('click', function(){
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
-
   })
+
+  $('.top-nav  .nav-link').on('click', function(){
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  })
+
+
+  $('nav a[href*="#"]').on('click', function(){
+    $('html,body').animate({
+      scrollTop:$($(this).attr('href')).offset()-100
+    }, 2000);
+  });
+
+  $('#up').on('click', function(){
+    $('html,body').animate({
+      scrollTop:0
+    }, 2000);
+  });
+
+
 });
 
+$('.skill-percentage').each(function(){
+  var $this = $(this);
+  var per = $this.attr('per');
+  $this.css("width", per+'%');
+  $({animatedValue: 0}).animate({animatedValue: per},{
+    duration: 1000,
+    step:function(){
+      $this.attr('per', Math.floor(this.animatedValue))
+    },
+    complete:function(){
+      $this.attr('per', Math.floor(this.animatedValue) + '%')
+    }
+  })
+});
